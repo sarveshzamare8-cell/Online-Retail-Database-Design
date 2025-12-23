23fb0318d28b53ed1426c2c31f080728509888be
Sarvesh <sarveshzamare8@gmail.com>
2025-12-23 17:38:08 +0530

    Update sample_data.sql

--- scripts/sample_data.sql
+++ scripts/sample_data.sql
@@ -1,28 +1,29 @@
--- Insert customers
+--Day_3
+
+
 INSERT INTO customers (first_name, last_name, email, phone) VALUES
 ('Amit', 'Sharma', 'amit.sharma@email.com', '9876543210'),
 ('Neha', 'Verma', 'neha.verma@email.com', '9123456780'),
 ('Rahul', 'Patel', 'rahul.patel@email.com', '9988776655');
 
--- Insert products
+
 INSERT INTO products (product_name, category, price, stock_quantity) VALUES
 ('Wireless Mouse', 'Electronics', 799.00, 50),
 ('Laptop Backpack', 'Accessories', 1499.00, 30),
 ('Bluetooth Headphones', 'Electronics', 2999.00, 20);
 
--- Insert orders
 INSERT INTO orders (customer_id, status) VALUES
 (1, 'Completed'),
 (2, 'Pending'),
 (3, 'Completed');
 
--- Insert order items
+
 INSERT INTO order_items (order_id, product_id, quantity, unit_price) VALUES
 (1, 1, 2, 799.00),
 (1, 2, 1, 1499.00),
 (3, 3, 1, 2999.00);
 
--- Insert payments
+
 INSERT INTO payments (order_id, amount, payment_method) VALUES
 (1, 3097.00, 'Credit Card'),
 (3, 2999.00, 'UPI');
@@ -32,3 +33,4 @@ SELECT * FROM products;
 SELECT * FROM orders;
 SELECT * FROM order_items;
 SELECT * FROM payments;
+
