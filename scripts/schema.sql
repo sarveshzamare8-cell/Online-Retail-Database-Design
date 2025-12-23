beaa674eb624717ce8504f81fb4692eb5b3fa3fd
Sarvesh <sarveshzamare8@gmail.com>
2025-12-23 17:30:03 +0530

    Update schema.sql

---
+++ scripts/schema.sql
@@ -0,0 +1,35 @@
+CREATE TABLE customers (
+    customer_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50),
+    email VARCHAR(100) UNIQUE NOT NULL,
+    phone VARCHAR(15),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE products (
+    product_id SERIAL PRIMARY KEY,
+    product_name VARCHAR(100) NOT NULL,
+    category VARCHAR(50),
+    price DECIMAL(10,2) CHECK (price > 0),
+    stock_quantity INT CHECK (stock_quantity >= 0)
+);
+CREATE TABLE orders (
+    order_id SERIAL PRIMARY KEY,
+    customer_id INT REFERENCES customers(customer_id),
+    order_date DATE DEFAULT CURRENT_DATE,
+    status VARCHAR(20)
+);
+CREATE TABLE order_items (
+    order_item_id SERIAL PRIMARY KEY,
+    order_id INT REFERENCES orders(order_id),
+    product_id INT REFERENCES products(product_id),
+    quantity INT CHECK (quantity > 0),
+    unit_price DECIMAL(10,2) CHECK (unit_price > 0)
+);
+CREATE TABLE payments (
+    payment_id SERIAL PRIMARY KEY,
+    order_id INT REFERENCES orders(order_id),
+    payment_date DATE DEFAULT CURRENT_DATE,
+    amount DECIMAL(10,2) CHECK (amount > 0),
+    payment_method VARCHAR(30)
+);
